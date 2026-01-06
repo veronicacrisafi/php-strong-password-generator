4f8834bde81ed6a2b68962c43211a0c679e4fb3a
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-01-06 17:31:28 +0100

    Aggiunta la logica per generare la password solo se i parametri sono validi e non ci sono errori

--- index.php
+++ index.php
@@ -22,6 +22,12 @@ $selezionaSimboli = isset($_GET['symbols']) ? $_GET['symbols'] : '';
 $pswLength = isset($_GET['pswlength']) ? $_GET['pswlength'] : '';
 $validazioneDati = validaCheckBox($selezionaLettere, $selezionaNumeri, $selezionaSimboli);
 
+// qui !empty($_GET) controlla che i parametri in GET non siano vuoti
+// mentre empty($errore) controlla che non ci siano errori quindi che la variabile $errore sia vuota
+if (!empty($_GET) && empty($errore)) {
+    $passwordGenerata = generatorePassword($pswLength, $checkRipetizioneCaratteri, !empty($selezionaLettere), !empty($selezionaNumeri), !empty($selezionaSimboli));
+}
+
 // Validazione lunghezza password e presenza selezione checkbox
 //controllo con empty che è una funzione che guarda se una variabile è vuota o meno
 //controllo con is_numeric che è una funzione che guarda che l'utente abbia rispettato il range della lunghezza numerica
