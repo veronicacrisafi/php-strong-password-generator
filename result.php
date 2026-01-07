f3b126a4e9011780a2c6101e4c59a259aff6e06e
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-01-07 22:49:55 +0100

    ottimizzata la visualizzazione della password generata con un layout centrato

--- result.php
+++ result.php
@@ -2,7 +2,6 @@
 require_once './functions.php';
 include_once './header.php';
 ?>
-
 <?php
 $errore = '';
 $selezionaLettere = isset($_GET['letter']) ? $_GET['letter'] : '';
@@ -11,7 +10,6 @@ $selezionaSimboli = isset($_GET['symbols']) ? $_GET['symbols'] : '';
 $pswLength = isset($_GET['pswlength']) ? $_GET['pswlength'] : '';
 $checkRipetizioneCaratteri = (isset($_GET['checkradio']) && $_GET['checkradio'] === 'Sì');
 $validazioneDati = validaCheckBox($selezionaLettere, $selezionaNumeri, $selezionaSimboli);
-
 // Validazione lunghezza password e presenza selezione checkbox
 //controllo con empty che è una funzione che guarda se una variabile è vuota o meno
 //controllo con is_numeric che è una funzione che guarda che l'utente abbia rispettato il range della lunghezza numerica
@@ -22,20 +20,19 @@ if (!empty($_GET)) {
         $errore = 'La lunghezza della password deve essere un numero tra 12 e 15.';
     }
 };
-
 // qui !empty($_GET) controlla che i parametri in GET non siano vuoti
 // mentre empty($errore) controlla che non ci siano errori quindi che la variabile $errore sia vuota
 if (!empty($_GET) && empty($errore)) {
     $passwordGenerata = generatorePassword($pswLength, $checkRipetizioneCaratteri, !empty($selezionaLettere), !empty($selezionaNumeri), !empty($selezionaSimboli));
 }
 ?>
-
- <?php
-    if (!empty($passwordGenerata) && empty($errore)) {
-        echo '<div class= "alert alert-success mt-3">' . 'La tua password supersicura è: ' . $passwordGenerata . '</div>';
-    };
-    ?>
-
+<?php
+if (!empty($passwordGenerata) && empty($errore)) {
+    echo '<div class="d-flex justify-content-center mt-3">';
+    echo '<div class="alert alert-success">' . 'La tua password supersicura è: ' . $passwordGenerata . '</div>';
+    echo '</div>';
+}
+?>
 <?php
 if (!empty($errore)) {
     echo '<div class="alert alert-danger mt-3">' . $errore . '</div>';
