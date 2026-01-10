cfd7cb069fc05a8421640b7e14346c13bd810335
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-01-10 20:12:58 +0100

    aggiunto il supporto per nuovi font e migliorato il layout della pagina principale; creato il file style.css per la gestione degli stili

--- header.php
+++ header.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&family=DynaPuff:wght@400..700&family=Fascinate+Inline&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Password Generator</title>
 </head>
 
--- index.php
+++ index.php
@@ -16,91 +16,73 @@ require_once './result.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&family=DynaPuff:wght@400..700&family=Fascinate+Inline&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Password Generator</title>
 </head>
 
-<body>
-    <div class="container">
-        <h1 class="text-center">Strong Password Generator</h1>
-        <h2 class="text-center">Genera una password sicura</h2>
+<body class="bg-black bg-gradient">
+    <div class="container border border-3 border-white rounded mt-5 mb-5 bg-dark bg-gradient">
+        <h1 class="text-center text-light mt-5 mb-4">Strong Password Generator</h1>
+        <h2 class="text-center text-light mb-5">Genera una password sicura</h2>
         <?php
         if ($errore) {
             echo '<div class="alert alert-danger">' . $errore .  '</div>';
         }
         ?>
         <form action="./result.php" method="GET">
-            <div class="row g-3 align-items-center justify-content-center">
-                <div class="col-auto">
-                    <label for="pswlength">Lunghezza password</label>
+            <div class="row justify-content-around">
+                <div class="col-4 mt-5">
+                    <label for="pswlength" class="form-label text-light">Lunghezza password</label>
                 </div>
-                <div class="col-auto">
-                    <input type="number" id="pswlength" class="form-control" name="pswlength" value="12" min="12" max="15">
+                <div class="col-4 mb-4 mt-5">
+                    <input type="number" id="pswlength" class="form-control text-center" name="pswlength" value="" min="12" max="15" placeholder="min:12 max:15">
                 </div>
-                <div class="row g-3 align-item-center justify-content-center">
-                    <div class="col-auto">
-                        <label for="">Consenti ripetizione di uno o più caratteri: </label>
+            </div>
+            <div class="row justify-content-around">
+                <div class="col-4">
+                    <label class="form-label text-light">Consenti ripetizione di uno o più caratteri:</label>
+                </div>
+                <div class="col-4 mb-4">
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" name="checkradio" id="checkyes" value="Sì" <?php if (isset($_GET['checkradio']) && $_GET['checkradio'] === 'Sì') echo 'checked'; ?>>
+                        <label for="checkyes" class="form-check-label text-light">Sì</label>
                     </div>
-                    <div class="col-auto">
-                        <!-- Per le checkradio è importante dare lo stesso name in quanto fanno parte della stessa scelta e l'utente può scegliere solo una delle due e così php riceve solo il valore selezionato-->
-                        <div class="form-check">
-                            <input type="radio" class="form-check-input" name="checkradio" id="checkyes" value="Sì" <?php if (isset($_GET['checkradio']) && $_GET['checkradio'] === 'Sì') echo 'checked'; ?>>
-                            <?php
-                            $checkRipetizioneCaratteri = (isset($_GET['checkradio']) && $_GET['checkradio'] === 'Sì');
-                            ?>
-                            <label for="checkyes" class="form-check-label">Sì</label>
-                        </div>
-                        <div class="form-check">
-                            <input type="radio" class="form-check-input" name="checkradio" id="checkno" value="No" <?php if (isset($_GET['checkradio']) && $_GET['checkradio'] === 'No') echo 'checked'; ?>>
-                            <?php
-                            $checkNonRipetizioneCaratteri = (isset($_GET['checkradio']) && $_GET['checkradio'] === 'No')
-                            ?>
-                            <label for="checkno" class="form-check-label">No</label>
-                        </div>
-                        <!-- Per le checkbox non serve dare lo stesso name in quanto l'utente può dare scelte multiple in questo modo php riceve le varie scelte selezionate-->
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="letterLowercase" id="letterLowercase" name="letterLowercase" <?php if (!empty($selezionaLettere)) echo 'checked'; ?>>
-                            <?php
-                            $selezionaLettereMinuscole = isset($_GET['letterLowercase']) ? $_GET['letterLowercase'] : '';
-                            ?>
-                            <label class="form-check-label" for="letterLowercase">
-                                Lettere minuscole
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="letterUppercase" id="letterUppercase" name="letterUppercase" <?php if (!empty($selezionaLettere)) echo 'checked'; ?>>
-                            <?php
-                            $selezionaLettereMaiuscole = isset($_GET['letterUppercase']) ? $_GET['letterUppercase'] : '';
-                            ?>
-                            <label class="form-check-label" for="letterUppercase">
-                                Lettere maiuscole
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="number" id="number" name="number" <?php if (!empty($selezionaNumeri)) echo 'checked'; ?>>
-                            <?php
-                            $selezionaNumeri = isset($_GET['number']) ? $_GET['number'] : ''
-                            ?>
-                            <label class="form-check-label" for="number">
-                                Numeri
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="symbols" id="symbols" name="symbols" <?php if (!empty($selezionaSimboli)) echo 'checked'; ?>>
-                            <?php
-                            $selezionaSimboli = isset($_GET['symbols']) ? $_GET['symbols'] : ''
-                            ?>
-                            <label class="form-check-label" for="symbols">
-                                Simboli
-                            </label>
-                        </div>
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" name="checkradio" id="checkno" value="No" <?php if (isset($_GET['checkradio']) && $_GET['checkradio'] === 'No') echo 'checked'; ?>>
+                        <label for="checkno" class="form-check-label text-light">No</label>
                     </div>
                 </div>
             </div>
-            <div class="row justify-content-center">
+            <div class="row justify-content-around">
+                <div class="col-4">
+                    <label class="form-label text-light">Caratteri ammessi:</label>
+                </div>
+                <div class="col-4 mb-5">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="letterLowercase" id="letterLowercase" name="letterLowercase" <?php if (!empty($selezionaLettereMinuscole)) echo 'checked'; ?>>
+                        <label class="form-check-label text-light" for="letterLowercase">Lettere minuscole</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="letterUppercase" id="letterUppercase" name="letterUppercase" <?php if (!empty($selezionaLettereMaiuscole)) echo 'checked'; ?>>
+                        <label class="form-check-label text-light" for="letterUppercase">Lettere maiuscole</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="number" id="number" name="number" <?php if (!empty($selezionaNumeri)) echo 'checked'; ?>>
+                        <label class="form-check-label text-light" for="number">Numeri</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="symbols" id="symbols" name="symbols" <?php if (!empty($selezionaSimboli)) echo 'checked'; ?>>
+                        <label class="form-check-label text-light" for="symbols">Simboli</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center mb-5">
                 <div class="col-auto">
-                    <button type="submit" class="btn btn-primary btn-sm">Invia</button>
-                    <!-- per il tasto annulla utilizzato il link che fa si che cliccandolo riporti alla pagina iniziale vuota -->
-                    <a href="index.php" class="btn btn-secondary btn-sm">Annulla</a>
+                    <button type="submit" class="btn btn-success btn-sm me-5 px-5">Invia</button>
+                    <a href="index.php" class="btn btn-danger btn-sm px-5">Annulla</a>
                 </div>
             </div>
             <?php
